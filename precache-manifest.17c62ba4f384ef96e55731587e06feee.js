908594344719ea59a6fa9cb1a6a6f865a2b69ae5
viktr 666 <49025707+viktr1432@users.noreply.github.com>
2019-12-17 22:52:15 +0300

    test

---
+++ precache-manifest.17c62ba4f384ef96e55731587e06feee.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df317a4a1c79067ca934e4756dffc486",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08b95b9fcbfbd44e3a34",
+    "url": "/static/css/main.e4d89a7a.chunk.css"
+  },
+  {
+    "revision": "e980e4cfe54e5ddf9136",
+    "url": "/static/js/2.5a87c93c.chunk.js"
+  },
+  {
+    "revision": "08b95b9fcbfbd44e3a34",
+    "url": "/static/js/main.75852e14.chunk.js"
+  },
+  {
+    "revision": "7325379478a821a86d1c",
+    "url": "/static/js/runtime-main.41256360.js"
+  },
+  {
+    "revision": "358e14407e40b539c5235bd11e447b1e",
+    "url": "/static/media/bg-320x393.358e1440.jpg"
+  },
+  {
+    "revision": "a70d27c9dc3eceec57d676791150e6d6",
+    "url": "/static/media/bg-768x622.a70d27c9.jpg"
+  },
+  {
+    "revision": "995bd2b48ea5cfa3dfb23dad4934614d",
+    "url": "/static/media/bg-desktop.995bd2b4.jpg"
+  },
+  {
+    "revision": "a956e5ef75eb6c4c46fb8251f7e8cc5f",
+    "url": "/static/media/bg-xl.a956e5ef.jpg"
+  },
+  {
+    "revision": "120ef9987012fe3556542272f37628ec",
+    "url": "/static/media/caret-down.120ef998.svg"
+  },
+  {
+    "revision": "2963787969ffcb9cc7324947abf540d5",
+    "url": "/static/media/css.29637879.svg"
+  },
+  {
+    "revision": "d882d855ad0c93a45176c40e0dacf8ce",
+    "url": "/static/media/html.d882d855.svg"
+  },
+  {
+    "revision": "cf53a810c6682334aa9a2e46a4b86ce4",
+    "url": "/static/media/javascript.cf53a810.svg"
+  },
+  {
+    "revision": "3b500131c4e9086a78a4490f6abc3f07",
+    "url": "/static/media/line-menu.3b500131.svg"
+  },
+  {
+    "revision": "10919850f9b0d50925a13d7db5d3eb21",
+    "url": "/static/media/logo.10919850.svg"
+  },
+  {
+    "revision": "3cdf78971c869b16b95152be7b6922e5",
+    "url": "/static/media/man-laptop-v1.3cdf7897.svg"
+  },
+  {
+    "revision": "1097427c8475728f4fdd7fa1b56f85d2",
+    "url": "/static/media/man-laptop-v2.1097427c.svg"
+  },
+  {
+    "revision": "2afa77717943906cc812fb6a7a97aeee",
+    "url": "/static/media/man-mobile.2afa7771.svg"
+  },
+  {
+    "revision": "1ad621857f42c14827fd09eda99ecd22",
+    "url": "/static/media/third-block-bg-1024x755.1ad62185.jpg"
+  },
+  {
+    "revision": "1129b934627f3c8747512c1baf31d7f1",
+    "url": "/static/media/third-block-bg-2560x692.1129b934.jpg"
+  },
+  {
+    "revision": "f051999e94176ab17a7be5131f0bd0a2",
+    "url": "/static/media/third-block-bg-320x1031.f051999e.jpg"
+  },
+  {
+    "revision": "0d709404de41ad2fe241e0ef770da89c",
+    "url": "/static/media/third-block-bg-768x878.0d709404.jpg"
+  }
+]);
